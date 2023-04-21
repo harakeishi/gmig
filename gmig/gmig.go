999c3b18e84bd79fb774fc82b4ae4eeae2a45677
harakeishi <hara.for.work@gmail.com>
2023-04-21 23:48:56 +0900

    select resultの保存

--- gmig/gmig.go
+++ gmig/gmig.go
@@ -17,6 +17,7 @@ type table struct {
 	name   string
 	masks  []mask
 	wheres []where
+	result map[string][]string
 }
 
 type mask struct {
@@ -33,6 +34,7 @@ type where struct {
 func (w where) build() string {
 	return fmt.Sprintf("%s %s %s", w.key, w.operator, w.value)
 }
+
 func (t table) getWhereStatement() string {
 	var WhereStatement []string
 	for _, v := range t.wheres {
@@ -40,6 +42,7 @@ func (t table) getWhereStatement() string {
 	}
 	return strings.Join(WhereStatement, ",")
 }
+
 func (t table) checkMask(s string) (string, bool) {
 	for _, v := range t.masks {
 		if v.key == s {
@@ -48,14 +51,17 @@ func (t table) checkMask(s string) (string, bool) {
 	}
 	return "", false
 }
+
 func (t table) createSelectSQL() string {
 	return fmt.Sprintf("SELECT * FROM %s WHERE %s", t.name, t.getWhereStatement())
 }
+
 func (t table) exec(db *sqlx.DB) {
 	rows, err := db.Queryx(t.createSelectSQL())
 	if err != nil {
 		log.Fatal(err)
 	}
+	t.result = make(map[string][]string)
 	data := map[string]interface{}{}
 	for rows.Next() {
 		rows.MapScan(data)
@@ -65,13 +71,16 @@ func (t table) exec(db *sqlx.DB) {
 			key = append(key, i)
 			if val, ok := t.checkMask(i); ok {
 				value = append(value, val)
+				t.result[i] = append(t.result[i], val)
 			} else {
 				value = append(value, fmt.Sprintf("%s", v))
+				t.result[i] = append(t.result[i], fmt.Sprintf("%s", v))
 			}
 		}
 		fmt.Printf("insert INTO %s (%s) VALUES (%s);\n", t.name, strings.Join(key, ","), strings.Join(value, ","))
 	}
 }
+
 func Gmig() {
 	selectSet := selectSet{
 		tables: []table{
